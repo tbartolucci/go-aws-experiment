4122d5f672da1b3de6865f0f8e154673850800fc
Tom Bartolucci <tbartolucci@gmail.com>
2018-05-23 19:36:19 -0400

    Cognito in sync

--- terraform/cognito.tf
+++ terraform/cognito.tf
@@ -11,6 +11,33 @@ resource "aws_cognito_user_pool" "photos-app-pool" {
     require_symbols   = true
     require_uppercase = true
   }
+
+  schema {
+    attribute_data_type      = "String"
+    developer_only_attribute = false
+    mutable                  = true
+    name                     = "name"
+    required                 = true
+
+    string_attribute_constraints {
+      min_length = 0
+      max_length = 2048
+    }
+  }
+
+  schema {
+    attribute_data_type      = "String"
+    developer_only_attribute = false
+    mutable                  = true
+    name                     = "email"
+    required                 = true
+
+    string_attribute_constraints {
+      min_length = 0
+      max_length = 2048
+    }
+  }
+
 }
 
 resource "aws_cognito_user_pool_client" "photos-app-web" {
@@ -18,4 +45,10 @@ resource "aws_cognito_user_pool_client" "photos-app-web" {
 
   user_pool_id = "${aws_cognito_user_pool.photos-app-pool.id}"
   explicit_auth_flows = ["ADMIN_NO_SRP_AUTH"]
+  read_attributes = ["given_name", "email_verified", "zoneinfo", "website", "preferred_username",
+    "name", "locale", "phone_number", "family_name", "birthdate", "middle_name", "phone_number_verified",
+    "profile", "picture", "address", "gender", "updated_at", "nickname", "email" ]
+  write_attributes = ["given_name", "zoneinfo", "website", "preferred_username",
+    "name", "locale", "phone_number", "family_name", "birthdate", "middle_name",
+    "profile", "picture", "address", "gender", "updated_at", "nickname", "email" ]
 }
